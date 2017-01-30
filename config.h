80e13d3b1b2c7e4f2ef85851753fe9dad0546acf
h4u5 <taylorlgroves@gmail.com>
2017-01-30 12:39:31 -0700

    Adding code for extended IB performance tests.

---
+++ config.h
@@ -0,0 +1,111 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Enable CUDA feature */
+/* #undef CUDA_PATH */
+
+/* Enable CUDA feature */
+/* #undef HAVE_CUDA */
+
+/* Enable DC feature */
+/* #undef HAVE_DC */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Enable endian conversion */
+#define HAVE_ENDIAN 1
+
+/* Define to 1 if you have the <infiniband/verbs_exp.h> header file. */
+/* #undef HAVE_INFINIBAND_VERBS_EXP_H */
+
+/* Define to 1 if you have the <infiniband/verbs.h> header file. */
+#define HAVE_INFINIBAND_VERBS_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `ibverbs' library (-libverbs). */
+#define HAVE_LIBIBVERBS 1
+
+/* Define to 1 if you have the `rdmacm' library (-lrdmacm). */
+#define HAVE_LIBRDMACM 1
+
+/* Have MASKED ATOMICS in verbs_exp header */
+/* #undef HAVE_MASKED_ATOMICS */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Have ODP support */
+/* #undef HAVE_ODP */
+
+/* Enable RAW_ETH_TEST */
+#define HAVE_RAW_ETH 1
+
+/* Enable RAW_ETH_TEST_EXP */
+/* #undef HAVE_RAW_ETH_EXP */
+
+/* Enable RAW_ETH_TEST_REG */
+#define HAVE_RAW_ETH_REG 1
+
+/* Have RSS in verbs_exp header */
+/* #undef HAVE_RSS_EXP */
+
+/* Enable SCIF link Layer */
+/* #undef HAVE_SCIF */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Have verbs_exp header */
+/* #undef HAVE_VERBS_EXP */
+
+/* Enable XRCD feature */
+#define HAVE_XRCD 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "perftest"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "linux-rdma@vger.kernel.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "perftest"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "perftest 5.33"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "perftest"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "5.33"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "5.33"
